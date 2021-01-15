9d3ad903763933389d73ea3c286e2dc6442fdd6a
kirkby <kirkby@neighbor.com>
2021-01-14 20:37:36 -0700

    progress!

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -9,11 +9,33 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      pokemon: data
+      pokemon: []
     };
   }
 
+  componentDidMount() {
+    console.log(`bk: App.js: compnentDidMount`);
+    // the DOM is renderd and availabel for us to query!!!!!
+    // call and API and asynchronously update
+    setTimeout(() => {
+      this.setState({ pokemon: data })
+    }, 4000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(`bk: App.js: CDU: prevProps, prevState: `, prevProps, prevState)
+    console.log(`bk: App.js: CDU: this.state: `, this.state)
+    if (prevProps.username !== this.props.username) {
+      console.log(`bk: CDU with username being changed`);
+    }
+
+    if (prevState.pokemon !== this.state.pokemon) {
+      console.log(`bk: CDU with pokemon state changing`);
+    }
+  }
+
   render() {
+    console.log(`bk: App.js: render(): App renders`);
     return (
       <div className="App">
         <Pokemon pokemon={this.state.pokemon} />
--- review/src/AppFunc.js
+++ review/src/AppFunc.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from "react";
 
-const AppFunc = () => {
+const AppFunc = ({ username }) => {
   const [name, setName] = useState("Warren");
   const handleClick = () => {
     setName("Allison");
   };
   console.log("APPFUNC: Component Constructed");
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("APPFUNC: Component Mounted");
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("APPFUNC: Component Updated");
   });
+
+  useEffect(() => {
+    console.log("APPFUNC: username from props changed");
+  }, [username])
 
   console.log("APPFUNC: Component Rendered");
   return (
     <div>
       <h1>Hello {name}!</h1>
-  
+
       <button onClick={handleClick}>MAKE IT ALLISON!</button>
     </div>
   );
