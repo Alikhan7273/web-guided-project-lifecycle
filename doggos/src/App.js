b47cebf7c787d1e2ed73f897c9a2b78a0f49ac4f
kirkby <kirkby@neighbor.com>
2021-01-14 21:18:02 -0700

    progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-class App extends React.Component {
+class App extends Component {
   state = {
     dogs: [],
     newBreed: 'hound',
